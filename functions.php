3c3002a793d7f1964f660e821264326f7b350a3a
Djurdjica Jovanovic <jovanovic.djurdjica@gmail.com>
2023-02-06 01:45:25 +0100

    Sitne ispravke

--- functions.php
+++ functions.php
@@ -73,75 +73,9 @@ function getCalender($year = '', $month = ''){
             </div> 
         </aside> 
          
-        <section class="calendar__days"> 
-            <section class="calendar__top-bar"> 
-                <span class="top-bar__days">Mon</span> 
-                <span class="top-bar__days">Tue</span> 
-                <span class="top-bar__days">Wed</span> 
-                <span class="top-bar__days">Thu</span> 
-                <span class="top-bar__days">Fri</span> 
-                <span class="top-bar__days">Sat</span> 
-                <span class="top-bar__days">Sun</span> 
-            </section> 
+       
              
-            <?php  
-                $dayCount = 1; 
-                $eventNum = 0; 
-                 
-                echo '<section class="calendar__week">'; 
-                for($cb=1;$cb<=$boxDisplay;$cb++){ 
-                    if(($cb >= $currentMonthFirstDay || $currentMonthFirstDay == 1) && $cb <= ($totalDaysOfMonthDisplay)){ 
-                        // Current date 
-                        $currentDate = $dateYear.'-'.$dateMonth.'-'.$dayCount; 
-                         
-                        // Get number of events based on the current date 
-                        global $mysqli; 
-                        $result = $mysqli->query("SELECT title FROM events WHERE date = '".$currentDate."' AND status = 1"); 
-                        $eventNum = $result->num_rows; 
-                         
-                        // Define date cell color 
-                        if(strtotime($currentDate) == strtotime(date("Y-m-d"))){ 
-                            echo ' 
-                                <div class="calendar__day today" onclick="getEvents(\''.$currentDate.'\');"> 
-                                    <span class="calendar__date">'.$dayCount.'</span> 
-                                    <span class="calendar__task calendar__task--today">'.$eventNum.' Events</span> 
-                                </div> 
-                            '; 
-                        }elseif($eventNum > 0){ 
-                            echo ' 
-                                <div class="calendar__day event" onclick="getEvents(\''.$currentDate.'\');"> 
-                                    <span class="calendar__date">'.$dayCount.'</span> 
-                                    <span class="calendar__task">'.$eventNum.' Events</span> 
-                                </div> 
-                            '; 
-                        }else{ 
-                            echo ' 
-                                <div class="calendar__day no-event" onclick="getEvents(\''.$currentDate.'\');"> 
-                                    <span class="calendar__date">'.$dayCount.'</span> 
-                                    <span class="calendar__task">'.$eventNum.' Events</span> 
-                                </div> 
-                            '; 
-                        } 
-                        $dayCount++; 
-                    }else{ 
-                        if($cb < $currentMonthFirstDay){ 
-                            $inactiveCalendarDay = ((($totalDaysOfMonth_Prev-$currentMonthFirstDay)+1)+$cb); 
-                            $inactiveLabel = 'expired'; 
-                        }else{ 
-                            $inactiveCalendarDay = ($cb-$totalDaysOfMonthDisplay); 
-                            $inactiveLabel = 'upcoming'; 
-                        } 
-                        echo ' 
-                            <div class="calendar__day inactive"> 
-                                <span class="calendar__date">'.$inactiveCalendarDay.'</span> 
-                                <span class="calendar__task">'.$inactiveLabel.'</span> 
-                            </div> 
-                        '; 
-                    } 
-                    echo ($cb%7 == 0 && $cb != $boxDisplay)?'</section><section class="calendar__week">':''; 
-                } 
-                echo '</section>'; 
-            ?> 
+           
         </section> 
     </main> 
  
