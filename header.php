3106591d413afff6d74bba14378f46e353c21c92
Đurđica Jovanović <jovanovic.djurdjica@hmail.com>
2023-02-12 21:31:38 +0100

    kalendar

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
             <a href="unosnovogfilma.php">Unos novih filmova</a>
             <a href="repertoar.php">Reperotar</a>
             <a href="pretraga.php">Pretraga</a>
+           
             <a href="javascript:void(0);" class="icon" onclick="myFunction()">
                 <i class="fa fa-bars"></i>
             </a>
--- repertoar.php
+++ repertoar.php
@@ -4,7 +4,7 @@
 include "server/konekcija.php";
 include "server/domain/zanr.php";
 include "server/domain/film.php";
-require_once 'functions.php';
+
 
 $order = '';
 
@@ -93,8 +93,65 @@ $filmovi=Film::vratiSve($mysqli,$order);
         <div class="col-md-2"></div>
     </div>
 
-    <div id="calendar_div">
-		<?php echo getCalender(); ?>
+<div  id="kalendar"  style="text-align: center;">
+    <div id="calendar_div"   >
+		
+        <?php
+require_once 'class.calendar.php';
+$phpCalendar = new PHPCalendar ();
+?>
+
+<html>
+<head>
+<link href="izgled.css" type="text/css" rel="stylesheet" />
+<title>PHP Calendar</title>
+</head>
+<body>
+<div id="body-overlay"><div><img src="img/loading.gif" width="64px" height="64px"/></div></div>
+<div id="calendar-html-output">
+<?php
+$calendarHTML = $phpCalendar->getCalendarHTML();
+echo $calendarHTML;
+?>
+</div>
+
+<script src="jquery-1.11.2.min.js" type="text/javascript"></script>
+<script>
+$(document).ready(function(){
+	$(document).on("click", '.prev', function(event) { 
+		var month =  $(this).data("prev-month");
+		var year =  $(this).data("prev-year");
+		getCalendar(month,year);
+	});
+	$(document).on("click", '.next', function(event) { 
+		var month =  $(this).data("next-month");
+		var year =  $(this).data("next-year");
+		getCalendar(month,year);
+	});
+	$(document).on("blur", '#currentYear', function(event) { 
+		var month =  $('#currentMonth').text();
+		var year = $('#currentYear').text();
+		getCalendar(month,year);
+	});
+});
+function getCalendar(month,year){
+	$("#body-overlay").show();
+	$.ajax({
+		url: "calendar-ajax.php",
+		type: "POST",
+		data:'month='+month+'&year='+year,
+		success: function(response){
+			setInterval(function() {$("#body-overlay").hide(); },500);
+			$("#calendar-html-output").html(response);	
+		},
+		error: function(){} 
+	});
+	
+}
+</script>
+</div>
+</body>
+</html>
 	</div>
     
 </body>
